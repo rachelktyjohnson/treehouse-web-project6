4618ee8b4c91211d6e5628863024d6dcbb9096fb
Rachel Johnson <rachelktyjohnson@gmail.com>
2021-07-14 21:31:11 +1000

    display a page

--- js/script.js
+++ js/script.js
@@ -1,30 +1,39 @@
-/*
-Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
-*/
-
-
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
+let items_per_page = 9;
 
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+function showPage(list, page) {
+    let i_start = (page * items_per_page) - items_per_page;
+    let i_end = page * items_per_page;
 
+    let student_list = document.querySelector('.student-list');
+    student_list.innerHTML = "";
+    for (let i = 0; i < list.length; i++) {
+        if (i >= i_start && i < i_end) {
+            let card = `
+                <li class="student-item cf">
+                    <div class="student-details">
+                        <img class="avatar" src="${data[i].picture.thumbnail}" alt="Profile Picture">
+                        <h3>${data[i].name.first} ${data[i].name.last}</h3>
+                        <span class="email">${data[i].email}</span>
+                    </div>
+                    <div class="joined-details">
+                        <span class="date">Joined ${data[i].registered.date}</span>
+                    </div>
+                </li>
+            `;
+            student_list.insertAdjacentHTML("beforeend",card);
+        }
+    }
+}
 
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
-
 
 
 // Call functions
